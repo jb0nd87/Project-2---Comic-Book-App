de32a27e4a0b836993acef28a029d9c64747705a
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-10-02 12:21:17 -0700

    comic app

--- comicsearch/src/App.js
+++ comicsearch/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Form from './components/Form';
+import ComicInfo from './components/ComicInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [comicData, setComicData] = useState({});
+
+	const getComic = (characters) => {
+		fetch(`https://comicvine.gamespot.com/api/characters/?api_key=976a615610f5757cbf1e12ceba7d6e5db194e594&sort=name&format=json`)
+			.then((response) => response.json())
+			.then((data) => {
+				setComicData(data);
+			});
+	};
+
+	useEffect(() => {
+		getComic('Iron Man');
+	}, []);
+
+	const handleSubmit = (characters) => {
+		getComic(characters);
+	};
+
+	return (
+		<div className='App'>
+			<Form handleSubmit={handleSubmit} />
+			<ComicInfo comicData={comicData} />
+		</div>
+	);
 }
 
 export default App;
