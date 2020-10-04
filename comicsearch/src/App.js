b2f73ebad37d01a43b43fbae02c013db559e247f
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-10-04 12:43:17 -0700

    comic app

--- comicsearch/src/App.js
+++ comicsearch/src/App.js
@@ -2,30 +2,63 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
 import ComicInfo from './components/ComicInfo';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
 	const [comicData, setComicData] = useState({});
+	const [comicAPIResults, setComicAPIResults] = useState([]);
+	const [formInput, setFormInput] = useState(null);
 
 	const getComic = (characters) => {
-		fetch(`https://comicvine.gamespot.com/api/characters/?api_key=976a615610f5757cbf1e12ceba7d6e5db194e594&sort=name&format=json`)
+		fetch(
+			`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=976a615610f5757cbf1e12ceba7d6e5db194e594&format=json`
+		)
 			.then((response) => response.json())
 			.then((data) => {
-				setComicData(data);
+				setComicAPIResults(data.results);
+				// console.log('this is results', data.results)
 			});
 	};
 
 	useEffect(() => {
-		getComic('Iron Man');
+		getComic();
 	}, []);
 
-	const handleSubmit = (characters) => {
-		getComic(characters);
+	const handleSubmit = (input) => {
+		// getComic(characters);
+		setFormInput(input);
 	};
 
 	return (
 		<div className='App'>
-			<Form handleSubmit={handleSubmit} />
-			<ComicInfo comicData={comicData} />
+			<nav>
+				<Link to='/Home'>
+					<h2>Home</h2>
+				</Link>
+				<Link to='/Comics'>
+					<h2>Comics</h2>
+				</Link>
+				<Link to='/About'>
+					<h2>About</h2>
+				</Link>
+			</nav>
+			<main>
+				<Switch>
+					<Route exact path='/Home'>
+						<Home />
+					</Route>
+					<Route path='/About'>
+						<About />
+					</Route>
+				</Switch>
+			</main>
+			{/* <div>This is the input value from the form: {formInput}</div> */}
+			<Form handleSubmit={handleSubmit} formInput={formInput} />
+			{comicAPIResults.map((comic) => {
+				return <ComicInfo comicData={comic} otherProp={'date'} />;
+			})}
 		</div>
 	);
 }
