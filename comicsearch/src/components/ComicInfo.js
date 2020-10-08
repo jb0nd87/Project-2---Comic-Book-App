62094a55645c636379a667be1d75e239301f5192
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-10-08 14:24:29 -0700

    comic app

--- comicsearch/src/components/ComicInfo.js
+++ comicsearch/src/components/ComicInfo.js
@@ -7,10 +7,14 @@ const ComicInfo = (props) => {
 
 	return (
 		<div>
-			<h1>Name: {comic.name}</h1>
-			<h3>Aliases: {comic.aliases}</h3>
-			<img src={comic.image.medium_url} alt={comic.icon_url} />
-			<h3>Description: {comic.deck}</h3>
+			<ul>
+				<li>{comic.name}</li>
+				<li>Aliases: {comic.aliases}</li>
+				<li>
+					<img src={comic.image.medium_url} alt={comic.icon_url} />
+				</li>
+				<li>Description: {comic.deck}</li>
+			</ul>
 		</div>
 	);
 };
