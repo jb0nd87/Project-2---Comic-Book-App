974b3607bd1eef750eb1a0979a57ebff139c1d69
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-10-08 22:24:46 -0700

    comic app

--- comicsearch/src/components/Header.js
+++ comicsearch/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
 			<>
 				<Navbar fixed='top' />
 				<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-					<Navbar.Brand href='#home'>Comics!</Navbar.Brand>
+					<Navbar.Brand href='home'>Comics!</Navbar.Brand>
 					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 					<Navbar.Collapse id='responsive-navbar-nav'>
 						<Nav className='mr-auto'></Nav>
